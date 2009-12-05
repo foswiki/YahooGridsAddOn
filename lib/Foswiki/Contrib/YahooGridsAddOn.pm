1cf98403d537de1594c33411beacc4a2925f60ea
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-12-05 09:22:51 +0000

    Item2459: doc corrections
    git-svn-id: http://svn.foswiki.org/trunk/YahooGridsAddOn@5729 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/YahooGridsAddOn.pm
+++ lib/Foswiki/Contrib/YahooGridsAddOn.pm
@@ -1,4 +1,4 @@
-package Foswiki::Contrib::PersonalInfoAddOn;
+package Foswiki::Contrib::YahooGridsAddOn;
 
 use vars qw( $VERSION $RELEASE );
 # This should always be $Rev: 2157 $ so that Foswiki can determine the
@@ -9,6 +9,6 @@ $VERSION = '$Rev: 2157 $';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '1.5';
+$RELEASE = '1.0';
 
 1;
